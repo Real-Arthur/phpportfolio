3d1f6d1a6189f5664f0ab3fee6614bd996aabebc
Arthur Carson <rthrcrsn@gmail.com>
2021-04-25 15:17:23 -0500

    added some about

--- about.php
+++ about.php
@@ -22,6 +22,10 @@
   foreach ($pictures as &$interest) {
     echo "<img src=$interest class='interests-pics w3-animate-fading' width='auto' height='500'/>";
   }
+  echo 
+  "
+  <h3>An avid film buff and television lover who's not afraid to go outside and break a sweat. I like biking around the lakes of Minneapolis and playing pickle ball most of all. I am also a huge fan of soccer especially Liverpool and Minnesota United. I loved watching The Loons at the amazing Allianz Field as often as possible. YNWA! Scarves Up!</h3>
+  "
 ?>
 
 
